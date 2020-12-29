0f056987f7b9cbfb64d61ae26e652be997281e29
routhj <@MNP19021992@JS>
2020-12-29 06:10:31 +0100

    Added Chart using chart.js

--- src/app/components/chart-module/chart-module.component.ts
+++ src/app/components/chart-module/chart-module.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'node_modules/chart.js'
 
 @Component({
   selector: 'app-chart-module',
@@ -10,6 +11,27 @@ export class ChartModuleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+var LineChart = new Chart('myLineChart', {
+    type: 'line',
+    data: {
+        labels: ['10.00', '10.05', '10.10', '10.15', '10.20', '10.25', 
+        '10.30', '10.35', '10.40', '10.45', '10.50', '10.55', '11.00'],
+        datasets: [{
+            label: '# of Votes',
+            data: [10, 19, 3, 5, 2, 3],
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
   }
 
 }
